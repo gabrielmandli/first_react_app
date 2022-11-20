a9b002783f4c747f046275c2e855e3e2c434b561
mandlga <gabriel.mandli@msg.group>
2022-11-20 21:52:01 +0100

    Creating ToDo List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Todo from "./components/Todo.js";
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React"/>
+      <Todo text="Master React"/>
+      <Todo text="Explore the full React course"/>
+    </div>
   );
 }
 
