ee49967a9d5221b79802128695ced6b420141659
mandlga <gabriel.mandli@msg.group>
2022-11-27 22:23:08 +0100

    finished fast course

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,30 @@
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>;
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-8deec-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>New Meetup Page</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
